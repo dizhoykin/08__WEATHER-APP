8f65811a9f520a83a5d331fef5293edb0b0dec5d
dizhoykin <dizhoykin@gmail.com>
2022-01-01 17:48:03 +0700

    Processing

---
+++ app.js
@@ -0,0 +1,26 @@
+const daysOfWeekMap = {
+    0: 'MON',
+    1: 'TUE',
+    2: 'WED',
+    3: 'THU',
+    4: 'FRI',
+    5: 'SAT',
+    6: 'SUN',
+}
+
+const iconNameToSizeMap = {
+    cloudy: { width: 264, height: 166},
+    sunny: { width: 208, height: 213},
+    stormy: { width: 246, height: 187},
+    snowy: { width: 230, height: 196},
+    partlycloudy: {width: 230, height:209},
+    rainy: { width: 160, height: 222},
+}
+
+console.log(Date());
+
+const days = document.querySelectorAll('.day-of-week');
+const dates = document.querySelectorAll('.date');
+
+
+console.log(daysArray);
